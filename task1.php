cd4a77f397454466f3d880f541967164700a1f05
webik123 <vadim.sw@hotmail.com>
2015-01-20 16:30:57 +0200

    Commit

--- task1.php
+++ task1.php
@@ -1,31 +1,3 @@
-<?
-// $output = '';
-// if($_SERVER['REQUEST_METHOD']=='POST'){
-// 	// Проверить все ли поля пришли?
-// 	$n1 = (int)$_POST['num1'];
-// 	$n2= (int)$_POST['num2'];
-// 	$op = trim(strip_tags($_POST['operator']));
-// 	$output = "$n1 $op $n2 = ";
-// 	switch ($op) {
-// 		case '+':
-// 			$output .= $n1 + $n2; break;
-// 		case '-':
-// 			$output .= $n1 - $n2; break;
-// 		case '*':
-// 			$output .= $n1 * $n2; break;
-// 		case '/':
-// 			if($n2 === 0)
-// 				$output = 'Деление на 0 запрещено!';
-// 			 else 
-// 			 	$output .= $n1 / $n2;
-// 			 break;
-// 		default: $output = "Неизвестный оператор '$op'";
-// 	}
-// }
-// if($output)
-// 	echo "<h3>Результат:" . $output "</h3>";
-
-?>
 			<form action='task1.php' method= 'post'>
 				<label>Name:</label><br />
 				<input name='name' type='text'/><br />
@@ -44,17 +16,19 @@ $n1 = trim(strip_tags($_POST['name']));
 $n2 = (int)trim(strip_tags($_POST['num']));
 
 
-if (!empty($_POST)) {
- 	print('<pre>' . print_r($_POST, TRUE) . '</pre>');
- }
+// if (!empty($_POST)) {
+//  	print('<pre>' . print_r($_POST, TRUE) . '</pre>');
+//  }
 
- echo "Privit " . $_POST['name'] . ", do penzii vam ostalos: " . $_POST['num'] . " age<br>";
 
+$a = 55 - $_POST['num'];
+$b = 60 - $_POST['num'];
  if ($_POST['browser'] == 'Women'){
- 	echo "Respected Women";
+ 	echo "Respected Women, " . "do penzii vam ostalos: " . $a;
  }
  	else 
- 		echo "Respected Men";
+ 		echo "Respected Men, " . "do penzii vam ostalos: " . $b;
+
 }
 
  ?>
