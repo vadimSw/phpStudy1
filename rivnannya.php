8053c00b7ce9a7725d8a627fbfe1f539f12824a9
webik123 <vadim.sw@hotmail.com>
2015-02-03 16:20:41 +0200

    Zaraz

--- rivnannya.php
+++ rivnannya.php
@@ -1,40 +1,4 @@
-<!-- Задача номер 1 -->
-<!-- <form action='rivnannya.php' method= 'post'>
-				<label>Chislo 1:</label><br>
-				<input size='2' name='one' type='text'/><br>
-				<label>Chislo 2:</label><br>
-				<input size='2' name='two' type='text'/><br>
-				<input type='submit' value='input'>
-			</form>
-<?
-$a = $_POST['one'];
-$b = $_POST['two'];
-$c = $a + $b;
-echo "Suma: " . $c;
-?>
 
-<hr> -->
-
- <!-- "Лінійне рівняння" -->
-
-<!-- <form action='rivnannya.php' method= 'post'>
-				<label>Rivnannya: </label>
-				<input size='2' name='one' type='text'/>
-				<label>* x + </label>
-				<input size='2' name='two' type='text'/>
-				<label>=</label>
-				<input size='2' name='free' type='text'/><br>
-				<input type='submit' value='input'>
-			</form>
-<?
-
-$one = $_POST['one'];
-$two = $_POST['two'];
-$free = $_POST['free'];
-$x = $free - $two;
-$y = $x / $one;
-echo "x = " . $y;
-?> -->
 
 
 <!-- Квадратне рівняння -->
@@ -51,16 +15,17 @@ echo "x = " . $y;
 			</form>
 
 <?
-$one = $_POST['one'];
-$two = $_POST['two'];
-$free = $_POST['free'];
-$four = $_POST['four'];
+$one = (int)$_POST['one'];
+$two = (int)$_POST['two'];
+$free = (int)$_POST['free'];
+$four = (int)$_POST['four'];
+
+
 $y = $two * $two;
 $x = 2 * $one;
 $z = $y - 4 * $one * $free;
 $d = sqrt($z);
-	$x1 = (-$two + $d) / $x;
-	$x2 = (-$two - $d) / $x;
+$x1 = (-$two + $d) / $x;
+$x2 = (-$two - $d) / $x;
 	echo "x1 = " . $x1 . ", x2 = " . $x2;
-
 ?>
